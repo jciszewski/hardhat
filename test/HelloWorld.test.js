503e48bd49ab1a48220114070b257ca0f2b8e534
jciszewski <119748937+jciszewski@users.noreply.github.com>
2022-12-03 14:34:37 -0500

    Added Unit Test for Hello World Contract
    Bonus #3

---
+++ test/HelloWorld.test.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+
+// Start test block
+describe('HelloWorld', function () {
+    before(async function () {
+        this.HelloWorld = await ethers.getContractFactory('HelloWorld');
+    });
+    beforeEach(async function () {
+        this.helloWorld = await this.HelloWorld.deploy();
+        await this.helloWorld.deployed();
+    });
+    // Test case
+    it('test return value is Hello World', async function () {
+        // Store a value
+        await this.helloWorld.helloWorld();
+
+        // Test if the returned value is the same one
+        // Note that we need to use strings to compare the 256 bit integers
+        expect((await this.helloWorld.helloWorld()).toString()).to.equal('Hello World');
+    });
+});
